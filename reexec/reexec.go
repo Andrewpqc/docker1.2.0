3af83f6ee9cfb7843c2214d2462f3dd8ed3cef1d
阿超 <m13636038496@163.com>
2018-08-01 12:06:03 +0800

    add commit for reexec/reexec.go
    add commit for reexec/exec.go

--- reexec/reexec.go
+++ reexec/reexec.go
@@ -7,7 +7,7 @@ import (
 	"path/filepath"
 )
 
-var registeredInitializers = make(map[string]func())
+var registeredInitializers = make(map[string]func()) //维护Initializers的包内全局map
 
 // Register adds an initialization func under the specified name
 func Register(name string, initializer func()) {
@@ -20,6 +20,9 @@ func Register(name string, initializer func()) {
 
 // Init is called as the first part of the exec process and returns true if an
 // initialization function was called.
+
+//docker启动时如果调用初始化函数，则会导致该函数返回true,从而导致主进程直接返回，仅仅执行初始化工作
+//否则，返回false,继续执行docker主进程
 func Init() bool {
 	initializer, exists := registeredInitializers[os.Args[0]]
 	if exists {
@@ -34,10 +37,21 @@ func Init() bool {
 // Self returns the path to the current processes binary
 func Self() string {
 	name := os.Args[0]
-
+	//filepath.Dir(),filepath.Base()
+	//可以将一个路径分解为目录和文件名两部分
+	
+	// 获取 path 中最后一个分隔符之前的部分（不包含分隔符）
+	//Dir(path string) string
+	// 获取 path 中最后一个分隔符之后的部分（不包含分隔符）
+	//Base(path string) string
+	
+	
+	//func LookPath(file string) (string, error) 
+	//LookPath在环境变量中查找可执行二进制文件，如果file中包含一个斜杠，
+	//则直接根据绝对路径或者相对本目录的相对路径去查找
 	if filepath.Base(name) == name {
 		if lp, err := exec.LookPath(name); err == nil {
-			name = lp
+			name = lp//这里的lp是找到的可执行文件的绝对路径
 		}
 	}
 
